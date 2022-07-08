6f80aee08a01ba5c3ca93f73503e6c209908e5a2
Jin409 <sualng123@gmail.com>
2022-07-08 16:39:17 +0900

    initial commit

---
+++ app1/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+app_name = 'app1'
+
+urlpatterns = [
+   # path('article/',article_list),
+   # path('detail/<int:pk>',article_detail),
+   path('article/',ArticleAPIView.as_view()),
+   path('detail/<int:id>',ArticleDetails.as_view()),
+   path('generic/article/<int:id>/',GenericAPIView.as_view()),
+]
